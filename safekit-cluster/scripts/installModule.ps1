934f77580ce26fadf1ef5422894218d18133ddf7
d6p <43806862+d6p@users.noreply.github.com>
2018-10-04 13:49:00 +0200

    Create SafeKit Cluster quickstart template

---
+++ safekit-cluster/scripts/installModule.ps1
@@ -0,0 +1,26 @@
+param(
+	[string]$safekitcmd,
+	[string]$MName
+)
+
+$module = [array] (ls "../*.safe")
+$idx=1
+$module | %{
+        if($_){
+			if($MName -and (length($MName) -gt 0)) {
+				if($idx -eq 1){
+					$modulename=$MName
+				}
+				else{
+					$modulename="$MName$idx"
+				}
+				$idx = $idx + 1
+				
+			}else{
+				$modulename = $($_.name.Replace(".safe",""))
+			}
+            
+            & $safekitcmd module install -m $modulename $_
+            & $safekitcmd -H "*" -E $modulename
+        } 
+} 
